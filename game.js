2023761f4c3da3e940aa189059e1ef7bbea88476
Daniel Guarin <daniel.guaring24@gmail.com>
2022-08-26 10:44:26 -0500

    implementacion de arreglos multidimensionales

--- game.js
+++ game.js
@@ -17,8 +17,27 @@ function startGame() {
 	console.log({ elementsSize, canvasSize });
 	game.textAlign = 'end';
 	game.font = `${elementsSize}px Verdana`;
-	for (let i = 1; i <= 10; i++) {
-		game.fillText(emojis['X'], elementsSize * i, elementsSize);
+
+	const map = maps[2];
+	console.log(map);
+	//Para conseguir las filas del mapa
+	mapRows = map.trim().split('\n'); // trim() limpia espacios al principio y al final, split() convierte un str a un array
+	// console.log(mapRows);
+	// Para quitar los espacios pendientes y dejar un arreglo de arreglos
+	const mapRowCols = mapRows.map((row) => {
+		return row.trim().split('');
+	});
+	console.log(mapRowCols);
+
+	for (let row = 1; row <= 10; row++) {
+		// game.fillText(emojis['X'], elementsSize * row, elementsSize * row);
+		for (let col = 1; col <= 10; col++) {
+			game.fillText(
+				emojis[mapRowCols[row - 1][col - 1]],
+				elementsSize * col,
+				elementsSize * row
+			);
+		}
 	}
 }
 
